bf2f854dbac87a4deb1ee3189ac2b46240562a90
Jeremy Chone <jeremy.chone@gmail.com>
2021-07-02 20:55:26 -0700

    . explosion (no animation)

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use player::PlayerPlugin;
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const LASER_SPRITE: &str = "laser_a_01.png";
 const ENEMY_SPRITE: &str = "enemy_a_01.png";
+const EXPLOSION_SHEET: &str = "explo_a_sheet.png";
 const SCALE: f32 = 0.5;
 const TIME_STEP: f32 = 1. / 60.;
 
@@ -20,6 +21,7 @@ pub struct Materials {
 	player: Handle<ColorMaterial>,
 	laser: Handle<ColorMaterial>,
 	enemy: Handle<ColorMaterial>,
+	explosion: Handle<TextureAtlas>,
 }
 struct WinSize {
 	#[allow(unused)]
@@ -35,6 +37,9 @@ struct PlayerReadyFire(bool);
 struct Laser;
 
 struct Enemy;
+
+struct Explosion;
+struct ExplosionToSpawn(Vec3);
 
 struct Speed(f32);
 impl Default for Speed {
@@ -59,6 +64,7 @@ fn main() {
 		.add_plugin(EnemyPlugin)
 		.add_startup_system(setup.system())
 		.add_system(laser_hit_enemy.system())
+		.add_system(explosion_to_spawn.system())
 		.run();
 }
 
@@ -66,6 +72,7 @@ fn setup(
 	mut commands: Commands,
 	asset_server: Res<AssetServer>,
 	mut materials: ResMut<Assets<ColorMaterial>>,
+	mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 	mut windows: ResMut<Windows>,
 ) {
 	let window = windows.get_primary_mut().unwrap();
@@ -74,10 +81,13 @@ fn setup(
 	commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
 	// create the main resources
+	let texture_handle = asset_server.load(EXPLOSION_SHEET);
+	let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 4, 4);
 	commands.insert_resource(Materials {
 		player: materials.add(asset_server.load(PLAYER_SPRITE).into()),
 		laser: materials.add(asset_server.load(LASER_SPRITE).into()),
 		enemy: materials.add(asset_server.load(ENEMY_SPRITE).into()),
+		explosion: texture_atlases.add(texture_atlas),
 	});
 	commands.insert_resource(WinSize {
 		w: window.width(),
@@ -112,7 +122,33 @@ fn laser_hit_enemy(
 
 				// remove the laser
 				commands.entity(laser_entity).despawn();
+
+				// spawn explosion to spawn
+				commands
+					.spawn()
+					.insert(ExplosionToSpawn(enemy_tf.translation.clone()));
 			}
 		}
+	}
+}
+
+fn explosion_to_spawn(
+	mut commands: Commands,
+	query: Query<(Entity, &ExplosionToSpawn)>,
+	materials: Res<Materials>,
+) {
+	for (explosion_spawn_entity, explosion_to_spawn) in query.iter() {
+		commands
+			.spawn_bundle(SpriteSheetBundle {
+				texture_atlas: materials.explosion.clone(),
+				transform: Transform {
+					translation: explosion_to_spawn.0,
+					..Default::default()
+				},
+				..Default::default()
+			})
+			.insert(Explosion);
+
+		commands.entity(explosion_spawn_entity).despawn();
 	}
 }
